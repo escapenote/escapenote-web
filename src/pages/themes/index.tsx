dba04c3c32f5fa09d3748dd8b7a8de51cf67bb45
gjjoo <joogeunjae@gmail.com>
2022-12-12 14:57:00 +0900

    fix: page link 맞지 않는 문제 수정

--- src/pages/themes/index.tsx
+++ src/pages/themes/index.tsx
@@ -8,7 +8,7 @@ const ThemeListPage = () => {
       <HeadPageMeta
         title="테마 리스트 - Escape Note"
         description="테마 리스트입니다."
-        pageUrl={`${process.env.NEXT_PUBLIC_URL}`}
+        pageUrl={`${process.env.NEXT_PUBLIC_URL}/themes`}
       />
 
       <Layout>
